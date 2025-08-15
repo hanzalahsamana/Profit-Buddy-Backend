4cbe435760b0cf1d02a26a29df26fcb6f75cf502
hanzalahsamana <hanzalahsamana789@gmail.com>
2025-08-15 22:02:05 +0500

    add apis

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const postRoutes = require("./Post.routes");
 const getRoutes = require("./Get.routes");
 
-router.use("/post", postRoutes);
 router.use("/get", getRoutes);
 
 module.exports = router;
